742928f6ce6e41abd98faf0856ed9c58d6b307b7
Ta Dat <tdblack.white@gmail.com>
2020-11-25 18:27:06 +0700

    Update homework Unit 16

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
+
+        <ul id="breadcrumb">
+          <li><a href="#"><span className="icon icon-home"> </span></a></li>
+          <li><a href="#"><span className="icon icon-beaker"> </span> Projects</a></li>
+          <li><a href="#"><span className="icon icon-double-angle-right" /> Breadcrumb</a></li>
+          <li><a href="#"><span className="icon icon-rocket"> </span> Getting started</a></li>
+          <li><a href="#"><span className="icon icon-arrow-down"> </span> Download</a></li>
+        </ul>
+
+        <section className="container">
+
+          <div className="questionBox">
+
+            <div className="questionContainer">
+
+              <header>
+                <h1 className="title">ReactJS Quiz</h1>
+                <div className="progressContainer">
+                  <progress className="progress" />
+                  <p>0% complete</p>
+                </div>
+              </header>
+
+              <h2 className="titleContainer title">What is the full form of HTTP?</h2>
+
+              <div className="optionContainer">
+                <div className="option">
+                  a. Hyper text transfer package
+              </div>
+                <div className="option">
+                  b. Hyper text transfer protocol
+              </div>
+                <div className="option">
+                  c. Hyphenation text test program
+              </div>
+                <div className="option">
+                  d. None of the above
+              </div>
+              </div>
+
+              <footer className="questionFooter">
+                <nav className="action">
+                  <a className="button">
+                    Back
+                </a>
+                  <a className="button">
+                    Next
+                </a>
+                </nav>
+              </footer>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
